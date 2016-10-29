dd1e3da351b37d82cc527e2a04f92e6092d29f9e
Mahesh Masale <masale123@hotmail.com>
2016-10-29 16:58:20 -0400

    added request calls

--- app.js
+++ app.js
@@ -1,10 +1,40 @@
-var express = require('express');
+var express = require("express");
+var http = require("http");
 var app = express();
+var path = require("path");
+var list = require('./request.js').Request;
+var logger = require("morgan");
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+var request_1 = require('superagent');
+
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/views/index.html'));
+  //__dirname : It will resolve to your project folder.
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.get('/about',function(req,res){
+  res.sendFile(path.join(__dirname+'/views/about.html'));
 });
+
+app.get('/sitemap',function(req,res){
+  res.sendFile(path.join(__dirname+'/views/sitemap.html'));
+});
+
+app.get('/request', function(req, response){
+    // run your request.js script
+    // when index.html makes the ajax call to www.yoursite.com/request, this runs
+    // you can also require your request.js as a module (above) and call on that:
+    //res.send(list.getList()); // try res.json() if getList() returns an object or array
+    console.log("called");
+    request_1.get('http://api.reimaginebanking.com/atms?key=4495856d0c2b6947ae81b8ab48802308').end(function(res){
+	    console.log(res.status);
+	    console.log(res.body); //do something
+	});
+
+});
+
+app.get("*", function(request, response) {
+  response.end("Saaale galat URL hai ye");
+});
+
+http.createServer(app).listen(1337);
