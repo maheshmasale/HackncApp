4d726ef7b952f97e34f8d51923c20d0d54df1074
Mahesh Masale <masale123@hotmail.com>
2016-10-29 18:46:58 -0400

    Full correction

--- app.js
+++ app.js
@@ -21,37 +21,15 @@ app.get('/sitemap',function(req,res){
   res.sendFile(path.join(__dirname+'/views/sitemap.html'));
 });
 
-/*
-app.get('/request', function(req, response){
-    // run your request.js script
-    // when index.html makes the ajax call to www.yoursite.com/request, this runs
-    // you can also require your request.js as a module (above) and call on that:
-    //res.send(list.getList()); // try res.json() if getList() returns an object or array
-    console.log("called");
-    request_1.get('http://api.reimaginebanking.com/atms?key=4495856d0c2b6947ae81b8ab48802308').end(function(res){
-      console.log(res.status);
-      console.log(res.body); //do something
-  });
-=======
-app.get('/lib/require-jquery.js',function(req,res){
-  res.sendFile(path.join(__dirname+'/lib/require-jquery.js'));
-});
-
-
-app.get('/lib/atm.js',function(req,res){
-  res.sendFile(path.join(__dirname+'/lib/atm.js'));
-});
->>>>>>> 08fd69e7833b877ad926074952da059a8d0d4825
 
 app.get('/lib/capital_one.js',function(req,res){
   res.sendFile(path.join(__dirname+'/lib/capital_one.js'));
 });
-*/
 
-app.get("*", function(request, response) {
-  response.end("Wrong URL!!");
+
+app.get('/lib/require-jquery.js',function(req,res){
+  res.sendFile(path.join(__dirname+'/lib/require-jquery.js'));
 });
-
 
 app.get('/lib/account.js',function(req,res){
   res.sendFile(path.join(__dirname+'/lib/account.js'));
@@ -94,14 +72,11 @@ app.get('/lib/withdrawal.js',function(req,res){
   res.sendFile(path.join(__dirname+'/lib/withdrawal.js'));
 });
 
-http.createServer(app).listen(1337);
-
-var express = require('express');
-var app = express();
-
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get("*", function(request, response) {
+  response.end("Wrong URL!!");
 });
+
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
